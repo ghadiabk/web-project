f131681820f878074fd1ff6798efef44054a9b52
RihamHarb025 <rihamharbhk2005@gmail.com>
2024-11-12 20:32:10 +0200

    added welcome page

---
+++ scriptWelcome.js
@@ -0,0 +1,17 @@
+let shopbtn=document.getElementById("shopbtn");
+let learnbtn=document.getElementById("learnbtn");
+
+
+function toProductPage(){
+    window.location.href="index.html";
+}
+shopbtn.addEventListener("click",function(){
+    toProductPage();
+})
+
+function toAboutUsPage(){
+    window.location.href="aboutUs.html";
+}
+learnbtn.addEventListener("click",function(){
+    toAboutUsPage();
+})
